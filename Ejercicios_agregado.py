a46929fdd125fa7156f776b953c61a7359176ccf
AlexisPE-15 <56988142+AlexisPE-15@users.noreply.github.com>
2019-11-02 00:38:39 -0500

    Add files via upload

---
+++ Ejercicios_agregado.py
@@ -0,0 +1,41 @@
+lista = []
+
+# Ejercicio 1
+lista.append("Pc")
+print (lista)
+
+# Ejercicio 2
+lista.append("pycharm")
+print (lista)
+
+# Ejercicio 3
+lista.append("mouse")
+print (lista)
+
+# Ejercicio 4
+lista.append("teclado")
+print (lista)
+
+# Ejercicio 5
+lista.append("pantalla")
+print (lista)
+
+# Ejercicio 6
+lista.append("cargador")
+print (lista)
+
+# Ejercicio 7
+lista.append("CPU")
+print (lista)
+
+# Ejercicio 8
+lista.append("audifonos")
+print (lista)
+
+# Ejercicio 9
+lista.append("IP")
+print (lista)
+
+# Ejercicio 10
+lista.append("placa madre")
+print (lista)
